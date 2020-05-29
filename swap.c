c692e6dac6a916f493945bed48a95249d06af7a9
Ohly-EXT <uia53124@contiwan.com>
2020-05-29 20:24:37 +0200

    added swap.c for playin' around.

---
+++ swap.c
@@ -0,0 +1,12 @@
+int main(void)
+{
+	int x = 29;
+	int y = 56;
+	printf("x is %i\n", x);
+	printf("y is %i\n", y);
+	
+	// swapping...
+
+	printf("x is now %i\n", x);
+	printf("y is now %i\n", y);
+}
